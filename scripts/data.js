a0a5581ee4bf378cc1b6ed6653ac2f5818c4fbd8
Luke Joshua <20785216@sun.ac.za>
2018-05-19 16:50:29 +0200

    format some scripts and removed location code

--- scripts/data.js
+++ scripts/data.js
@@ -1,3 +1,5 @@
+// still don't know what to do with this
+
 const fs = require('fs')
 const dirName = '../hadley_drive/data/'
 const fileList = fs.readdirSync(dirName)
@@ -6,9 +8,9 @@ const data = fileList.map(name => [...fs.readFileSync(dirName+name)])
 
 for(let i = 0; i < data.length; i++){
 	const outputName = `not_${fileList[i]}`
-	fs.writeFileSync(`../plus/${outputName}`, Buffer.from(not(data[i])))
+	fs.writeFileSync(`../plus/${outputName}`, Buffer.from(what(data[i])))
 }
 
-function not(a){
+function what(a){
 	return a.map(x => x+1)
 }
